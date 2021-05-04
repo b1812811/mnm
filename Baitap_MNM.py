842f0dbb371d36428facc5e601cbf24d6a299002
Quang B1812811 <quangb1812811@student.ctu.edu.vn>
2021-05-04 14:38:26 +0700

    Modify Baitap_MNM.py file

--- Baitap_MNM.py
+++ Baitap_MNM.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 
 print("Hello Teacher")
+print("--o----o---oooooo---o--------o--------oooooo--")
+print("--o----o---o--------o--------o--------o----o--")
+print("--o----o---o--------o--------o--------o----o--")
+print("--o----o---o--------o--------o--------o----o--")
+print("--oooooo---oooooo---o--------o--------o----o--")
+print("--o----o---o--------o--------o--------o----o--")
+print("--o----o---o--------o--------o--------o----o--")
+print("--o----o---o--------o--------o--------o----o--")
+print("--o----o---oooooo---oooooo---oooooo---oooooo--")
